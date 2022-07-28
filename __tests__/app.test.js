755a0fb6798ea49212fadd49a79d085d978df7d0
Pedro <pedro.chn01@gmail.com>
2022-07-28 14:29:06 -0700

    adding test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,16 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('horses routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should return a list of horses', async () => {
+    const res = await request(app).get('/Horses');
+    const expected = horses.map((horse) => {
+      return { id: horse.id, name: horse.name };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
