dd9ae0ad5c24ecdd96496a1aaf4f1a2c311e408d
Pedro <pedro.chn01@gmail.com>
2022-07-28 15:01:22 -0700

    added second test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,6 +14,18 @@ describe('horses routes', () => {
     });
     expect(res.body).toEqual(expected);
   });
+
+  it('/horses/:id should return hoses detail', async () => {
+    const res = await request(app).get('/horses/1');
+    const lucky = {
+      id: '1',
+      name: 'Lucky',
+      wins: '13',
+      born: '2012',
+    };
+    expect(res.body).toEqual(lucky);
+  });
+
   afterAll(() => {
     pool.end();
   });
